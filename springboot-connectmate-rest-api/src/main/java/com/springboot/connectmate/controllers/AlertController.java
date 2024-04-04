1b452a6f495aa9966e193b70008943e7a334cd62
AndreaAguilar <a01656200@tec.mx>
2024-04-03 19:56:29 -0600

    Update AlertController.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/AlertController.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/AlertController.java
@@ -1,4 +1,20 @@
 package com.springboot.connectmate.controllers;
 
+@RestController
+@RequestMapping ("/alerts")
 public class AlertController {
+    @GetMapping("/")
+    public ResponseEntity<List<AlertDTO>> getAlerts(){
+        return ResponseEntity.ok(alerts)
+    }
+    @PostMapping("/")
+    public ResponseEntity<AlertAddDTO> addAlert(@RequestBody AlertAddDTo newalerts) {
+        return ResponseEntity.ok (newalerts)
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<AlertModifyDTO> modifyAlert
+
+
+
 }
