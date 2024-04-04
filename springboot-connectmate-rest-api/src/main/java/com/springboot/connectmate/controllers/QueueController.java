8fa74392648cafcbf53609bc66b47a363622dbb9
MoyBadillo117 <A00834306@tec.mx>
2024-04-03 20:27:52 -0600

    Queue Controller
    Se añadió el queue controller

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/QueueController.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/QueueController.java
@@ -1,4 +1,20 @@
 package com.springboot.connectmate.controllers;
 
+@RestController
+@RequestMapping("/queue")
 public class QueueController {
+    @GetMapping
+    public String getQueue() {
+        return "GET request for /queue";
+    }
+
+    @GetMapping("/{id}")
+    public String getQueue(@PathVarible("id") int id) {
+        return "GET request for /queue/id";
+    }
+    @GetMapping("/{id}/agent")
+    public String getQueue() {
+        return "GET request for /queue/id/agent";
+    }
+
 }
