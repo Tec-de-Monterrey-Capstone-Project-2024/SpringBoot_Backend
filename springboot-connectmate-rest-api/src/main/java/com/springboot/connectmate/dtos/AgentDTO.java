e8dd7d46926a84d0a3e348adf8c3420e1d4c6b1a
Aram32mm <a01657142@tec.mx>
2024-04-07 21:35:35 -0600

    ENDPOINTS 3 Y 4 :+1:

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentDTO.java
@@ -9,4 +9,45 @@ import lombok.Data;
         description = "DTO for Agent"
 )
 public class AgentDTO {
+    private Long id;
+    @Schema(
+            description = "Name of the Agent",
+            example = "Jane Doe"
+    )
+    private String name;
+    @Schema(
+            description = "Email of the Agent",
+            example = "agent@example.com"
+    )
+    private String email;
+    @Schema(
+            description = "Password of the Agent",
+            example = "password"
+    )
+    private String password;
+    @Schema(
+            description = "Total calls handled by the Agent",
+            example = "500"
+    )
+    private Integer totalCallsHandled;
+    @Schema(
+            description = "Average handling time (AHT) in seconds",
+            example = "300"
+    )
+    private Integer averageHandlingTime;
+    @Schema(
+            description = "Customer satisfaction score (CSAT), from 1 to 5",
+            example = "4"
+    )
+    private Integer customerSatisfactionScore;
+    @Schema(
+            description = "First Call Resolution (FCR) rate, as a percentage",
+            example = "75"
+    )
+    private Integer firstCallResolutionRate;
+    @Schema(
+            description = "Availability status of the Agent",
+            example = "Available"
+    )
+    private String availabilityStatus;
 }
