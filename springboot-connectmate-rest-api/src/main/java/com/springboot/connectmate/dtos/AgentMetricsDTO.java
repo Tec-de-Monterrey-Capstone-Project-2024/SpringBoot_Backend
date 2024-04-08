39dd9e68011c5844e3447d7846f1299681aba13d
abeejita <a01655892@itesm.com>
2024-04-08 17:54:45 -0600

    Implemented the AgentMetricsDTO.

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentMetricsDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentMetricsDTO.java
@@ -1,7 +1,10 @@
 package com.springboot.connectmate.dtos;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data // Lombok annotation to create all the getters, setters, equals, hash, and toString methods based on the fields
 @Schema(
@@ -9,4 +12,34 @@ import lombok.Data;
         description = "DTO for Agent Metrics"
 )
 public class AgentMetricsDTO {
+    @Schema(
+            description = "Id of the Alert",
+            example = "1"
+    )
+    private Long id;
+
+    @Schema(
+            description = "Name of the Alert",
+            example = "Service Level"
+    )
+    private String name;
+
+    @Schema(
+            description = "Minimum threshold for the Alert",
+            example = "3"
+    )
+    private int minThreshold;
+
+    @Schema(
+            description = "Maximum threshold for the Alert",
+            example = "5"
+    )
+    private int maxThreshold;
+
+    @Schema(
+            description = "Date of the metric",
+            example = "2021-12-31T23:59:59"
+    )
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    private LocalDateTime metricDate;
 }
