6026f3bec052c207774e02b7ef55812f522c896f
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-04-09 13:28:43 -0600

    Change Suggestion to Task in all codes and endpoints

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/TaskController.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/TaskController.java
@@ -1,14 +1,89 @@
 package com.springboot.connectmate.controllers;
 
+import com.springboot.connectmate.dtos.TaskDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api/tasks")
+@RequestMapping("/api/task")
 @Tag(
         name = "Task REST API",
-        description = "CRUD REST API for Tasks"
+        description = "An API that have the CRUD services for tasks in the Call Center"
 )
 public class TaskController {
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Get the tasks successfully",
+                    content = {
+                        @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = TaskDTO.class))
+            }),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content
+            )
+    })
+    @Operation(summary = "Get all tasks for the Call Center")
+    @GetMapping
+    public ResponseEntity<List<TaskDTO>> getAllTasks(){
+        List<TaskDTO> response = new ArrayList<>();
+        TaskDTO task1 = new TaskDTO();
+        task1.setId(1L);
+        task1.setType(TaskDTO.TaskType.QUEUE);
+        task1.setStatus(TaskDTO.TaskStatus.TODO);
+        task1.setDescription("Not enough people on virtual floor.");
+        task1.setDetails("http://localhost:8080/api/task/1");
+        task1.setCreatedAt(LocalDateTime.now());
+
+        response.add(task1);
+        return ResponseEntity.ok(response);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                description = "Task updated successfully",
+                content = {
+                    @Content(
+                            schema = @Schema(implementation = TaskDTO.class))
+            }),
+            @ApiResponse(responseCode = "500",
+                description = "Internal Server Error",
+                content = @Content
+            )
+    })
+    @Operation(summary = "Modify one specific task by its identifier (ID)")
+    @PutMapping("/{taskId}")
+    public ResponseEntity<TaskDTO> updateTask(@PathVariable Long taskId, @RequestBody TaskDTO task){
+        task.setId(taskId);
+        task.setUpdatedAt(LocalDateTime.now());
+
+        return ResponseEntity.ok(task);
+    }
+
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204",
+                    description = "No content"
+            ),
+            @ApiResponse(responseCode = "500",
+                    description = "Internal Server Error",
+                    content = @Content
+            )
+    })
+    @Operation(summary = "Delete one task by Id in the database")
+    @DeleteMapping("/{taskId}")
+    public ResponseEntity<String> deleteTask(@PathVariable Long taskId){
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/TaskDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/TaskDTO.java
@@ -9,59 +9,59 @@ import java.time.LocalDateTime;
 
 @Data
 @Schema(
-        name = "General Suggestion",
-        description = "DTO for general suggestion"
+        name = "General Task",
+        description = "DTO for general tasks in the Call Center"
 )
-public class SuggestionDTO {
+public class TaskDTO {
     @Schema(
             name = "Identifier (ID)",
-            description = "Primary key (PK) for the entity of Suggestions",
+            description = "Primary key (PK) for the entity of Tasks",
             example = "1"
     )
     private Long id;
 
-    public enum SuggestionType{AGENT, QUEUE, OTHER}
+    public enum TaskType{AGENT, QUEUE, OTHER}
 
     @Schema(
             name = "Type",
-            description = "Type of the suggestion",
+            description = "Type of the task",
             example = "QUEUE"
     )
-    private SuggestionType type;
+    private TaskType type;
 
-    public enum SuggestionStatus{TODO, DONE}
+    public enum TaskStatus{TODO, DONE}
 
     @Schema(
             name = "Status",
-            description = "Status of the Suggestion (To Do or Done)",
+            description = "Status of the Task (To Do or Done)",
             example = "TODO"
     )
-    private SuggestionStatus status;
+    private TaskStatus status;
 
     @Schema(
             name = "Description",
-            description = "Text giving the title of the suggestion",
+            description = "Text giving the title of the task",
             example = "Not enough people on virtual floor"
     )
     private String description;
 
     @Schema(
             name = "Details",
-            description = "API's url giving the details for that suggestion",
-            example = "http://localhost:8080/api/suggestion/1"
+            description = "API's url giving the details for the task",
+            example = "http://localhost:8080/api/task/1"
     )
     private String details;
 
     @Schema(
             name = "Created At",
-            description = "Created At timestamp of the Suggestion",
+            description = "Created At timestamp of the Task",
             example = "2021-12-31T23:59:59"
     )
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
     private LocalDateTime createdAt;
 
     @Schema(
-            description = "Updated At timestamp of the Suggestion",
+            description = "Updated At timestamp of the Task",
             example = "2021-12-31T23:59:59"
     )
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
