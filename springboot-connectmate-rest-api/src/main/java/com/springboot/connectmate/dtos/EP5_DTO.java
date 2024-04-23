ac2c7eaa1bf72fd4acc4ed4f9f9faf3e0d94e948
Armando-Mandujano <119339269+Armando-Mandujano@users.noreply.github.com>
2024-04-22 21:54:24 -0600

    MetricAgentsDTO.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/EP5_DTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/EP5_DTO.java
@@ -1,59 +1,13 @@
 package com.example.demo.dto;
+import lombok.Data;
 
-public class EP5_DTO{
+@Data
+public class MetricAgentsDTO{
     private int id;
     private String name;
     private String description;
     private int limitInSeconds;
     private int answeredCalls;
     private int totalCalls;
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public int getLimitInSeconds() {
-        return limitInSeconds;
-    }
-
-    public int getAnsweredCalls() {
-        return answeredCalls;
-    }
-
-    public int getTotalCalls() {
-        return totalCalls;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setLimitInSeconds(int limitInSeconds) {
-        this.limitInSeconds = limitInSeconds;
-    }
-
-    public void setAnsweredCalls(int answeredCalls) {
-        this.answeredCalls = answeredCalls;
-    }
-
-    public void setTotalCalls(int totalCalls) {
-        this.totalCalls = totalCalls;
-    }
 
 }
