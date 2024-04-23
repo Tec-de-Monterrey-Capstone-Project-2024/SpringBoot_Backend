540f96c34554e932c9108eb7f17e2dc4f75320a8
Armando-Mandujano <119339269+Armando-Mandujano@users.noreply.github.com>
2024-04-22 21:55:20 -0600

    KPIsDTO.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/EP6_DTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/EP6_DTO.java
@@ -1,13 +1,13 @@
 package com.example.demo.dto;
 
-import java.util.Date;
-import java.util.List;
-
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
 
 //Este DTO trae todos los KPIs del Agente
+@Data
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
-public class EP6_DTO {
+public class KPIsDTO {
     private int id;
     private int SL;
     private int ACR;
@@ -15,69 +15,8 @@ public class EP6_DTO {
     private int OCCUPANCY;
     private int Schedule_Adherence;
     private int ASA;
-    private int AHA;
+    private int AHT;
+    private int ACW;
 
-    public int getId() {
-        return id;
-    }
 
-    public int getSL() {
-        return SL;
-    }
-
-    public int getACR() {
-        return ACR;
-    }
-
-    public int getFCR() {
-        return FCR;
-    }
-
-    public int getOCCUPANCY() {
-        return OCCUPANCY;
-    }
-
-    public int getSchedule_Adherence() {
-        return Schedule_Adherence;
-    }
-
-    public int getASA() {
-        return ASA;
-    }
-
-    public int getAHA() {
-        return AHA;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setSL(int SL) {
-        this.SL = SL;
-    }
-
-    public void setACR(int ACR) {
-        this.ACR = ACR;
-    }
-
-    public void setFCR(int FCR) {
-        this.FCR = FCR;
-    }
-
-    public void setOCCUPANCY(int OCCUPANCY) {
-        this.OCCUPANCY = OCCUPANCY;
-    }
-
-    public void setSchedule_Adherence(int schedule_Adherence) {
-        Schedule_Adherence = schedule_Adherence;
-    }
-
-    public void setASA(int ASA) {
-        this.ASA = ASA;
-    }
-
-    public void setAHA(int AHA) {
-        this.AHA = AHA;
-    }
 }
