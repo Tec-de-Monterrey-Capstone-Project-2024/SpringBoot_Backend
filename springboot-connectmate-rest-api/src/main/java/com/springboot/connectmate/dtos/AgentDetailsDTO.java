f2075b42c70fa8d0f7f752b7674f39640e4bd95f
Armando-Mandujano <119339269+Armando-Mandujano@users.noreply.github.com>
2024-04-22 21:56:13 -0600

    AgentDetailsDTO.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentDetailsDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentDetailsDTO.java
@@ -1,35 +1,14 @@
 package com.example.demo.dto;
 
 import java.util.List;
+import lombok.Data;
 
-//Este DTO Trae toda el ID del agente para que en el EP6 se organice todos los KPIs con el agente
+
+@Data
 public class AgentDetailsDTO {
     private int AgentId;
     private String Description;
 
-    private List<EP6_DTO> KPIs;
+    private List<KPIsDTO> KPIs;
 
-    public int getAgentId() {
-        return AgentId;
-    }
-
-    public String getDescription() {
-        return Description;
-    }
-
-    public List<EP6_DTO> getKPIs() {
-        return KPIs;
-    }
-
-    public void setAgentId(int agentId) {
-        AgentId = agentId;
-    }
-
-    public void setDescription(String description) {
-        Description = description;
-    }
-
-    public void setKPIs(List<EP6_DTO> KPIs) {
-        this.KPIs = KPIs;
-    }
 }
