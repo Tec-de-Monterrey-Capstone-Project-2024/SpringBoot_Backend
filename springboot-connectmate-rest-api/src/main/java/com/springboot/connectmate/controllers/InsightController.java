7bdf25f035c33343b31a0522e839e18f81845394
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-04-23 11:24:09 -0600

    Quit import of unused class 'ApiResponses' in InsightController.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/InsightController.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/controllers/InsightController.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
