3b7089c51b69f9810d66d7ced26608f63e7c02c5
Armando-Mandujano <119339269+Armando-Mandujano@users.noreply.github.com>
2024-04-23 20:31:10 -0600

    Update KPIsDTO.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/KPIsDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/KPIsDTO.java
@@ -8,14 +8,59 @@ import lombok.Data;
 @Data
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class KPIsDTO {
+
+    @Schema(
+            description = "Id of the KPI",
+            example = "1"
+    )
     private int id;
+
+    @Schema(
+            description = "KPI Service level",
+            example = "80"
+    )
     private int SL;
+
+    @Schema(
+            description = "KPI Abandon Call Rate",
+            example = "3"
+    )
     private int ACR;
+
+    @Schema(
+            description = "KPI First Call Resolution",
+            example = "80"
+    )
     private int FCR;
+
+    @Schema(
+            description = "KPI OCCUPANCY",
+            example = "85"
+    )
     private int OCCUPANCY;
+
+    @Schema(
+            description = "KPI Schedule Adherence",
+            example = "95"
+    )
     private int Schedule_Adherence;
+
+    @Schema(
+            description = "KPI Average Speed of Answer",
+            example = "30"
+    )
     private int ASA;
+
+    @Schema(
+            description = "KPI Average Handling Time",
+            example = "240"
+    )
     private int AHT;
+
+    @Schema(
+            description = "KPI After Call Work",
+            example = "100"
+    )
     private int ACW;
 
 
