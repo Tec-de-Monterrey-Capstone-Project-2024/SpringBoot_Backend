2f3cc4548e9f4acfd82bfebde7f8d793a7f18451
Armando-Mandujano <119339269+Armando-Mandujano@users.noreply.github.com>
2024-04-23 20:13:13 -0600

    Update MetricAgentsDTO.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/MetricAgentsDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/MetricAgentsDTO.java
@@ -3,11 +3,40 @@ import lombok.Data;
 
 @Data
 public class MetricAgentsDTO{
+    @Schema(
+            description = "Id of the Supervisor",
+            example = "1"
+    )
     private int id;
+    
+    @Schema(
+            description = "Name of the KPI",
+            example = "Service level"
+    )
     private String name;
+    
+    @Schema(
+            description = "KPI's description",
+            example = "Calls ansered within"
+    )
     private String description;
+    
+    @Schema(
+            description = "KPI's metrics",
+            example = "20"
+    )
     private int limitInSeconds;
+    
+    @Schema(
+            description = "KPI's metrics",
+            example = "80"
+    )
     private int answeredCalls;
+    
+    @Schema(
+            description = "KPI's metrics",
+            example = "90"
+    )
     private int totalCalls;
 
 }
