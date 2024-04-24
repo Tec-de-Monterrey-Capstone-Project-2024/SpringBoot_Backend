57ac0cf61a52a4a13440898ac7ff1ea1ac7f559a
Armando-Mandujano <119339269+Armando-Mandujano@users.noreply.github.com>
2024-04-23 20:24:58 -0600

    Update AgentDetailsDTO.java

--- springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentDetailsDTO.java
+++ springboot-connectmate-rest-api/src/main/java/com/springboot/connectmate/dtos/AgentDetailsDTO.java
@@ -6,9 +6,23 @@ import lombok.Data;
 
 @Data
 public class AgentDetailsDTO {
+
+    @Schema(
+            description = "Agent id",
+            example = "1"
+    )
     private int AgentId;
+    
+    @Schema(
+            description = "KPI",
+            example = "sl (service level)"
+    )
     private String Description;
 
+    @Schema(
+            description = "List of KPI's",
+            example = " {"id": 1,"sl": 80},{"id": 2,"acr": 3},{"id":3,"fcr":80},{"id":4,"occupancy":85},{"id":5,"schedule_Adherence":95},{"id":6,"asa":30},{"id":7,"aht":240},{"id":8,"acw":100}"
+    )
     private List<KPIsDTO> KPIs;
 
 }
