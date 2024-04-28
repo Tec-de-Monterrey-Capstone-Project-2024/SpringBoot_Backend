2cdd85473621888252ff2058a28d148b303bd395
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-04-28 02:35:27 -0600

    Services implemented for the Metrics controller, specially the average metrics for all agents in the Call Center

--- src/main/java/com/springboot/connectmate/services/MetricService.java
+++ src/main/java/com/springboot/connectmate/services/MetricService.java
@@ -1,6 +1,9 @@
 package com.springboot.connectmate.services;
 
+import com.springboot.connectmate.dtos.Metric.MetricDTO;
+
+import java.util.List;
+
 public interface MetricService {
-
-
+    List<MetricDTO> getContactCenterMetrics();
 }
--- src/main/java/com/springboot/connectmate/services/impl/MetricServiceImpl.java
+++ src/main/java/com/springboot/connectmate/services/impl/MetricServiceImpl.java
@@ -1,10 +1,17 @@
 package com.springboot.connectmate.services.impl;
 
+import com.springboot.connectmate.dtos.Metric.MetricDTO;
+import com.springboot.connectmate.enums.MetricCategory;
 import com.springboot.connectmate.repositories.MetricRepository;
 import com.springboot.connectmate.services.MetricService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MetricServiceImpl implements MetricService {
@@ -16,5 +23,23 @@ public class MetricServiceImpl implements MetricService {
     public MetricServiceImpl(MetricRepository metricRepository, ModelMapper mapper) {
         this.metricRepository = metricRepository;
         this.mapper = mapper;
+    }
+
+    @Override
+    @Transactional
+    public List<MetricDTO> getContactCenterMetrics(){
+        /* Get the data from a Store Procedure (SP) form our database server */
+        List<Object[]> results = metricRepository.getContactCenterMetrics();
+
+        /* Deserialization */
+        return results.stream()
+                .map(result -> {
+                    MetricDTO dto = new MetricDTO();
+                    dto.setCode((MetricCategory) result[0]);
+                    dto.setName((String) result[1]);
+                    dto.setValue((BigDecimal) result[2]);
+                    return dto;
+                })
+                .collect(Collectors.toList());
     }
 }
