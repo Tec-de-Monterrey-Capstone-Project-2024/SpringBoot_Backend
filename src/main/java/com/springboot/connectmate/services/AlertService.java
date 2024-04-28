3c638f7f983dfb7df462ca9fc829b94113297b36
Aram32mm <a01657142@tec.mx>
2024-04-28 02:23:16 -0600

    Alert DTO Design + Test Endpoint

--- src/main/java/com/springboot/connectmate/services/AlertService.java
+++ src/main/java/com/springboot/connectmate/services/AlertService.java
@@ -1,4 +1,7 @@
 package com.springboot.connectmate.services;
 
+import com.springboot.connectmate.dtos.Alert.AlertDTO;
+
 public interface AlertService {
+    AlertDTO getAlertById(Long alertId);
 }
