3c68f30dd11c67c1ad895e26d5a6e74d621a2f94
abeejita <a01655892@itesm.com>
2024-04-27 22:42:49 -0600

    Finished insights/queues endpoint.

--- src/main/java/com/springboot/connectmate/services/impl/InsightServiceImpl.java
+++ src/main/java/com/springboot/connectmate/services/impl/InsightServiceImpl.java
@@ -1,16 +1,21 @@
 package com.springboot.connectmate.services.impl;
 
 import com.springboot.connectmate.dtos.Insight.InsightDTO;
+import com.springboot.connectmate.enums.InsightStatus;
 import com.springboot.connectmate.models.Insight;
 import com.springboot.connectmate.repositories.InsightRepository;
 import com.springboot.connectmate.services.InsightService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class InsightServiceImpl implements InsightService {
 
-    private ModelMapper mapper;
+    private final ModelMapper mapper;
     private final InsightRepository insightRepository;
     public InsightServiceImpl(InsightRepository insightRepository, ModelMapper mapper) {
         this.insightRepository = insightRepository;
@@ -23,4 +28,22 @@ public class InsightServiceImpl implements InsightService {
         return mapper.map(insight, InsightDTO.class);
     }
 
+    @Override
+    @Transactional
+    public List<InsightDTO> getQueueInsights() {
+        List<Object[]> results = insightRepository.getQueueInsights();
+        return results.stream()
+                .map(result -> {
+                    InsightDTO dto = new InsightDTO();
+                    dto.setId((Long) result[0]);
+                    dto.setStatus(InsightStatus.valueOf((String) result[1]));
+                    dto.setSummaryTemplate((String) result[3]);
+                    dto.setSituationTemplate((String) result[4]);
+                    dto.setActionsTemplate((String) result[5]);
+                    dto.setThresholdBreachId((Long) result[6]);
+                    dto.setConstructedDescription((String)result[2] + (String)result[7]);
+                    return dto;
+                })
+                .collect(Collectors.toList());
+    }
 }
