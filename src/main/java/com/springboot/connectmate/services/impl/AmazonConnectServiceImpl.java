eaf813cc111329e124ffeb81dc6ce0d0487d968f
Aram32mm <a01657142@tec.mx>
2024-04-28 19:26:17 -0600

    Amazon Connect Creds (For Testing, No me vulneren porfavor). + Amazon Connect Exposed APIS

--- src/main/java/com/springboot/connectmate/services/impl/AmazonConnectServiceImpl.java
+++ src/main/java/com/springboot/connectmate/services/impl/AmazonConnectServiceImpl.java
@@ -5,8 +5,7 @@ import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.connect.AmazonConnect;
 import com.amazonaws.services.connect.AmazonConnectClientBuilder;
-import com.amazonaws.services.connect.model.ListInstancesRequest;
-import com.amazonaws.services.connect.model.ListInstancesResult;
+import com.amazonaws.services.connect.model.*;
 import com.springboot.connectmate.services.AmazonConnectService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,10 +15,14 @@ import java.util.stream.Collectors;
 
 @Service
 public class AmazonConnectServiceImpl implements AmazonConnectService {
+
+    @Value("${aws.accessKeyId}")
     private String accessKeyId;
 
+    @Value("${aws.secretKey}")
     private String secretKey;
 
+    @Value("${aws.region}")
     private String region;
 
     private AmazonConnect amazonConnectClient() {
@@ -38,5 +41,25 @@ public class AmazonConnectServiceImpl implements AmazonConnectService {
                 .map(instanceSummary -> "Instance ARN: " + instanceSummary.getArn() + ", Id: " + instanceSummary.getId())
                 .collect(Collectors.toList());
     }
+
+    @Override
+    public List<String> listQueues(String instanceId) {
+        ListQueuesRequest listQueuesRequest = new ListQueuesRequest().withInstanceId(instanceId);
+        ListQueuesResult listQueuesResult = amazonConnectClient().listQueues(listQueuesRequest);
+        return listQueuesResult.getQueueSummaryList().stream()
+                .map(queueSummary -> "Queue ARN: " + queueSummary.getArn() + ", Name: " + queueSummary.getName())
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<String> listAgents(String instanceId) {
+        ListAgentStatusesRequest listAgentStatusesRequest = new ListAgentStatusesRequest().withInstanceId(instanceId);
+        ListAgentStatusesResult listAgentStatusesResult = amazonConnectClient().listAgentStatuses(listAgentStatusesRequest);
+        return listAgentStatusesResult.getAgentStatusSummaryList().stream()
+                .map(agentSummary -> "Agent ARN: " + agentSummary.getArn() + ", Username: " + agentSummary.getName())
+                .collect(Collectors.toList());
+    }
+
+
 
 }
