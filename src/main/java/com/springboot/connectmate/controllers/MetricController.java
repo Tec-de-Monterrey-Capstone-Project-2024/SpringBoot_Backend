7987c4c7b4f0177b037949e683932b4729f93538
abeejita <a01655892@itesm.com>
2024-04-30 20:17:20 -0600

    Metrics endpoint implemented.

--- src/main/java/com/springboot/connectmate/controllers/MetricController.java
+++ src/main/java/com/springboot/connectmate/controllers/MetricController.java
@@ -1,12 +1,16 @@
 package com.springboot.connectmate.controllers;
 
+import com.springboot.connectmate.dtos.Insight.InsightDTO;
+import com.springboot.connectmate.dtos.Metric.ConnectMetricDTO;
 import com.springboot.connectmate.dtos.Metric.MetricDTO;
+import com.springboot.connectmate.enums.InsightStatus;
 import com.springboot.connectmate.services.MetricService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -40,5 +44,19 @@ public class MetricController {
     @GetMapping("/agents")
     public List<MetricDTO> getContactCenterMetrics(){
         return metricService.getContactCenterMetrics();
+    }
+
+    // Get all metrics for a single agent.
+    @Operation(
+            summary = "Get all the metrics for a single agent",
+            description = "Get all the metrics for a single agent in the contact center."
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Metrics fetched successfully"
+    )
+    @GetMapping("/agents/{agent_id}")
+    public List<ConnectMetricDTO> getAgentMetrics(@PathVariable Long agent_id){
+        return metricService.getAgentMetrics(agent_id);
     }
 }
--- src/main/java/com/springboot/connectmate/services/MetricService.java
+++ src/main/java/com/springboot/connectmate/services/MetricService.java
@@ -1,5 +1,6 @@
 package com.springboot.connectmate.services;
 
+import com.springboot.connectmate.dtos.Metric.ConnectMetricDTO;
 import com.springboot.connectmate.dtos.Metric.MetricDTO;
 import com.springboot.connectmate.dtos.Metric.MetricDescriptionDTO;
 
@@ -8,4 +9,5 @@ import java.util.List;
 public interface MetricService {
     MetricDescriptionDTO getMetricDescriptionById(Long metricId);
     List<MetricDTO> getContactCenterMetrics();
+    List<ConnectMetricDTO> getAgentMetrics(Long agentId);
 }
