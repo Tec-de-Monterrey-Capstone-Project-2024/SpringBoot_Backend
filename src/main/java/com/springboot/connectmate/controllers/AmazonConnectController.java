30a6131bd9d4bc03d6a66cf1af39cb2f3391ab92
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2024-05-13 01:09:55 -0600

    Partially documented DTO's and controllers

--- src/main/java/com/springboot/connectmate/controllers/AmazonConnectController.java
+++ src/main/java/com/springboot/connectmate/controllers/AmazonConnectController.java
@@ -2,6 +2,7 @@ package com.springboot.connectmate.controllers;
 
 import com.springboot.connectmate.dtos.AmazonConnect.*;
 import com.springboot.connectmate.services.AmazonConnectService;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,7 +11,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -28,34 +28,57 @@ public class AmazonConnectController {
         this.amazonConnectService = amazonConnectService;
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of instances fetched successfully"
+    )
     @GetMapping("/instances")
     public ResponseEntity<List<ConnectInstanceDTO>> listConnectInstances() {
         return ResponseEntity.ok(amazonConnectService.listConnectInstances());
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "Queue list of a given instance fetched successfully"
+    )
     @GetMapping("/instances/{instanceId}/queues")
     public ResponseEntity<List<ConnectQueueDTO>> listQueues(@PathVariable(name = "instanceId") String instanceId) {
         return ResponseEntity.ok(amazonConnectService.listQueues(instanceId));
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of users for a given instance fetched successfully"
+    )
     @GetMapping("/instances/{instanceId}/users")
     public ResponseEntity<List<ConnectUserDTO>> listUsers(@PathVariable(name = "instanceId") String instanceId) {
         return ResponseEntity.ok(amazonConnectService.listUsers(instanceId));
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of agent statues for a given instance fetched successfully"
+    )
     @GetMapping("/instances/{instanceId}/agents")
     public ResponseEntity<List<ConnectAgentDTO>> listAgents(@PathVariable(name = "instanceId") String instanceId) {
         return ResponseEntity.ok(amazonConnectService.listAgents(instanceId));
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of historical metrics for a given instance fetched successfully"
+    )
     @GetMapping("/instances/{instanceId}/historial-metrics")
     public ResponseEntity<List<String>> getHistoricalMetrics(@PathVariable(name = "instanceId") String instanceId) {
         return ResponseEntity.ok(amazonConnectService.getHistoricalMetrics(instanceId));
     }
 
+    @ApiResponse(
+            responseCode = "200",
+            description = "List of routing profiles for a given instance fetched successfully"
+    )
     @GetMapping("/instances/{instanceId}/routing-profiles")
     public ResponseEntity<List<ConnectRoutingProfileDTO>> getRoutingProfiles(@PathVariable(name = "instanceId") String instanceId) {
         return ResponseEntity.ok(amazonConnectService.listRoutingProfiles(instanceId));
     }
-
 }
