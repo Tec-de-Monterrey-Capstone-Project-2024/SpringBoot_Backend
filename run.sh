dc249d63f4a2cff886a350383aa067690cd7d13d
Aram32mm <a01657142@tec.mx>
2024-05-17 10:35:10 -0600

    mmm

--- run.sh
+++ run.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "PORT: ${PORT}"
-echo "DB_HOST: ${DB_HOST}"
-echo "DB_NAME: ${DB_NAME}"
-echo "DB_USER: ${DB_USER}"
-echo "DB_PASSWORD: ${DB_PASSWORD}"
-echo "AWS_ACCESS_KEY: ${AWS_ACCESS_KEY}"
-echo "AWS_SECRET_KEY: ${AWS_SECRET_KEY}"
-echo "AWS_REGION: ${AWS_REGION}"
+echo "PORT -> ${PORT}"
+echo "DB_HOST -> ${DB_HOST}"
+echo "DB_NAME -> ${DB_NAME}"
+echo "DB_USER -> ${DB_USER}"
+echo "DB_PASSWORD -> ${DB_PASSWORD}"
+echo "AWS_ACCESS_KEY -> ${AWS_ACCESS_KEY}"
+echo "AWS_SECRET_KEY -> ${AWS_SECRET_KEY}"
+echo "AWS_REGION -> ${AWS_REGION}"
 
 java -DPORT="${PORT}" \
   -DDB_HOST="${DB_HOST}" \
