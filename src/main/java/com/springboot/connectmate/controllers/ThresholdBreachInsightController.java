3ca47d46644d8dc990e0877cb23f5e1c6e9a89cf
MoyBadillo117 <A00834306@tec.mx>
2024-05-28 15:29:42 -0600

    Update ThresholdBreachInsightController.java
    Se implementó el @ApiResponse y el @operation además del @GetMapping o @PatchMaapping a los métodos del controlador

--- src/main/java/com/springboot/connectmate/controllers/ThresholdBreachInsightController.java
+++ src/main/java/com/springboot/connectmate/controllers/ThresholdBreachInsightController.java
@@ -1,5 +1,8 @@
 package com.springboot.connectmate.controllers;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import com.springboot.connectmate.dtos.Insight.ThresholdBreachInsightDTO;
 import com.springboot.connectmate.dtos.Insight.UpdateStatusDTO;
 import com.springboot.connectmate.enums.Status;
@@ -27,6 +30,13 @@ public class ThresholdBreachInsightController {
         this.service = service;
     }
 
+
+    @Operation(summary = "Get insights by status, connectItemId, or itemType", description = "Retrieve insights based on the provided parameters. If no parameters are provided, all insights are returned.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Invalid parameter"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @GetMapping
     public ResponseEntity<List<ThresholdBreachInsightDTO>> getInsights(
             @RequestParam(required = false) String status,
@@ -51,6 +61,13 @@ public class ThresholdBreachInsightController {
     }
 
 
+    @Operation(summary = "Update the status of an insight", description = "Update the status of a ThresholdBreachInsight by its ID.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Status updated successfully"),
+            @ApiResponse(responseCode = "404", description = "Insight not found"),
+            @ApiResponse(responseCode = "400", description = "Invalid status value"),
+            @ApiResponse(responseCode = "500", description = "Internal server error")
+    })
     @PatchMapping("/{id}/status")
     public ResponseEntity<ThresholdBreachInsightDTO> updateInsightStatus(@PathVariable Long id, @RequestBody UpdateStatusDTO updateStatusDTO) {
         ThresholdBreachInsightDTO updatedInsight = service.updateStatus(id, updateStatusDTO);
