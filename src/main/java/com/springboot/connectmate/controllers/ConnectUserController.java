68e20512c31ff85cb001a9d99526364c353caf31
fernando <a01642285@tec.mx>
2024-05-28 17:07:52 -0600

    get security profiles endpoint

--- src/main/java/com/springboot/connectmate/controllers/ConnectUserController.java
+++ src/main/java/com/springboot/connectmate/controllers/ConnectUserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.springboot.connectmate.dtos.AmazonConnect.UserRoleDTO;
 
 import java.util.*;
 
