cbaefe920e18ff9cd062de070cbfe5310bc130ea
Armando-Mandujano <A01655899@tec.mx>
2024-05-27 20:40:28 -0600

    Controller

--- src/main/java/com/springboot/connectmate/controllers/ThresholdBreachInsightController.java
+++ src/main/java/com/springboot/connectmate/controllers/ThresholdBreachInsightController.java
@@ -1,8 +1,20 @@
 package com.springboot.connectmate.controllers;
 
+import com.springboot.connectmate.dtos.AmazonConnect.InsightDTO;
+import com.springboot.connectmate.dtos.AmazonConnect.InsightStatusUpdateDTO;
+import com.springboot.connectmate.dtos.AmazonConnect.KpiDataDTO;
+import com.springboot.connectmate.dtos.AmazonConnect.ThresholdBreachInsightDTO;
+import com.springboot.connectmate.enums.ConnectMetricType;
+import com.springboot.connectmate.enums.Status;
+import com.springboot.connectmate.models.ThresholdBreachInsight;
+import com.springboot.connectmate.services.BedrockService;
 import com.springboot.connectmate.services.ThresholdBreachInsightService;
+import com.springboot.connectmate.services.impl.BedrockServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -14,9 +26,53 @@ import org.springframework.web.bind.annotation.*;
 public class ThresholdBreachInsightController {
 
     private final ThresholdBreachInsightService thresholdBreachInsightService;
+    private final BedrockService bedrockService;
 
     @Autowired
-    public ThresholdBreachInsightController(ThresholdBreachInsightService thresholdBreachInsightService) {
+    public ThresholdBreachInsightController(ThresholdBreachInsightService thresholdBreachInsightService, BedrockService bedrockService) {
         this.thresholdBreachInsightService = thresholdBreachInsightService;
+        this.bedrockService = bedrockService;
     }
+
+
+    @Operation(
+            summary = "Update Insight Status",
+            description = "Updates the Insight Status With a New Status"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "Status updated successfully"
+    )
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<String> updateInsightStatus(
+            @PathVariable Long id,
+            @RequestBody InsightStatusUpdateDTO statusUpdateDTO
+    ) {
+        Status newStatus = statusUpdateDTO.getNewStatus();
+        thresholdBreachInsightService.updateInsightStatus(id, newStatus);
+        return ResponseEntity.ok("Status updated successfully");
+    }
+
+
+    /*
+    @PostMapping("/generateAndSaveInsight")
+    public ResponseEntity<ThresholdBreachInsight> generateAndSaveInsight(
+            @RequestBody ThresholdBreachInsightDTO dto,
+            @RequestParam Double metricValue,
+            @RequestParam ConnectMetricType metricType,
+            @RequestParam String typeId) {
+
+        // Call the BedrockService to get the insight
+        InsightDTO insight = bedrockService.createInsight(dto);
+
+        // Fill in additional details from the request parameters
+        dto.setValue(metricValue);
+        dto.setConnectItemType(metricType);
+        dto.setConnectItemId(typeId);
+
+        // Generate and save the insight
+        ThresholdBreachInsight savedInsight = thresholdBreachInsightService.generateAndSaveInsight(dto, insight);
+        return ResponseEntity.ok(savedInsight);
+    }*/
+
 }
--- src/main/java/com/springboot/connectmate/services/impl/ThresholdBreachInsightServiceImpl.java
+++ src/main/java/com/springboot/connectmate/services/impl/ThresholdBreachInsightServiceImpl.java
@@ -1,10 +1,18 @@
 package com.springboot.connectmate.services.impl;
 
+import com.springboot.connectmate.dtos.AmazonConnect.InsightDTO;
+import com.springboot.connectmate.enums.*;
+import com.springboot.connectmate.exceptions.ResourceNotFoundException;
+import com.springboot.connectmate.models.Metric;
+import com.springboot.connectmate.models.ThresholdBreachInsight;
 import com.springboot.connectmate.repositories.ThresholdBreachInsightRepository;
 import com.springboot.connectmate.services.ThresholdBreachInsightService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ThresholdBreachInsightServiceImpl implements ThresholdBreachInsightService {
@@ -19,4 +27,39 @@ public class ThresholdBreachInsightServiceImpl implements ThresholdBreachInsight
         this.thresholdBreachInsightRepository = thresholdBreachInsightRepository;
         this.mapper = mapper;
     }
+
+
+    @Transactional
+    public void updateInsightStatus(Long insightId, Status newStatus) {
+        ThresholdBreachInsight insight = thresholdBreachInsightRepository.findById(insightId)
+                .orElseThrow(() -> new ResourceNotFoundException("ThresholdBreachInsight", "id", insightId));
+        insight.setStatus(newStatus);
+        thresholdBreachInsightRepository.save(insight);
+    }
+
+
+    /*
+    @Transactional
+    public ThresholdBreachInsight generateAndSaveInsight(InsightDTO insight, Double metricValue, ConnectMetricType metricType, String typeId) {
+
+        Metric metric = new Metric();
+        ThresholdBreachInsight thresholdBreachInsight = new ThresholdBreachInsight();
+        thresholdBreachInsight.setMetricCode(metric);
+        thresholdBreachInsight.setConnectItemId(typeId);
+        thresholdBreachInsight.setConnectItemType(metricType);
+        thresholdBreachInsight.setValue(metricValue);
+        thresholdBreachInsight.setOccurredAt(LocalDateTime.now());
+        thresholdBreachInsight.setStatus(Status.TO_DO);
+        thresholdBreachInsight.setInsightName(insight.getInsightName());
+        thresholdBreachInsight.setInsightSummary(insight.getInsightSummary());
+        thresholdBreachInsight.setInsightDescription(insight.getInsightDescription());
+        thresholdBreachInsight.setInsightActions(insight.getInsightActions());
+        thresholdBreachInsight.setInsightCategory(InsightPerformance.valueOf(insight.getInsightCategory().toUpperCase()));
+        thresholdBreachInsight.setInsightSeverity(InsightSeverity.LOW);
+        thresholdBreachInsight.setInsightRootCause(insight.getInsightRootCause());
+        thresholdBreachInsight.setInsightImpact(insight.getInsightImpact());
+        thresholdBreachInsight.setInsightPrevention(insight.getInsightPrevention());
+
+        return thresholdBreachInsightRepository.save(thresholdBreachInsight);
+    }*/
 }
