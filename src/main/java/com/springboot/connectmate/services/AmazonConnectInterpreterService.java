4f6caf38cc458422b2fcd3ab3ba455c47cc0651a
fernando-tecm <105445226+fernando-tecm@users.noreply.github.com>
2024-05-28 16:28:39 -0600

    Delete src/main/java/com/springboot/connectmate/services/AmazonConnectInterpreterService.java

--- src/main/java/com/springboot/connectmate/services/AmazonConnectInterpreterService.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.connectmate.services;
-
-import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.Map;
-
-public interface AmazonConnectInterpreterService {
-    Map<String, Map<String, Object>> getQueueUserCounts(String instanceId);
-}
