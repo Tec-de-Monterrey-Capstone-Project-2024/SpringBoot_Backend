da66bd8874028aa3044b670279e9aee9aeffcdee
MoyBadillo117 <A00834306@tec.mx>
2024-05-28 00:19:43 -0600

    Patch de status ThresholdBreachInsight
    Se implementó un nuevo endpoint de tipo patch

--- src/main/java/com/springboot/connectmate/services/ThresholdBreachInsightService.java
+++ src/main/java/com/springboot/connectmate/services/ThresholdBreachInsightService.java
@@ -1,6 +1,7 @@
 package com.springboot.connectmate.services;
 
 import com.springboot.connectmate.dtos.Insight.ThresholdBreachInsightDTO;
+import com.springboot.connectmate.dtos.Insight.UpdateStatusDTO;
 import com.springboot.connectmate.enums.ConnectMetricType;
 import com.springboot.connectmate.enums.Status;
 
@@ -11,4 +12,5 @@ public interface ThresholdBreachInsightService {
     List<ThresholdBreachInsightDTO> getInsightsByConnectItemId(String connectItemId);
     List<ThresholdBreachInsightDTO> getAllInsights();
     List<ThresholdBreachInsightDTO> getInsightsByItemType(ConnectMetricType connectItemType);
+    ThresholdBreachInsightDTO updateStatus(Long id, UpdateStatusDTO updateStatusDTO);
 }
