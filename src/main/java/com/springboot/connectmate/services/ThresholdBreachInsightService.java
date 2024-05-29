56c167057754e837f742135d9745d47fb8e216a1
Aram32mm <a01657142@tec.mx>
2024-05-28 22:44:17 -0600

    Moy Endpoints + My Quality

--- src/main/java/com/springboot/connectmate/services/ThresholdBreachInsightService.java
+++ src/main/java/com/springboot/connectmate/services/ThresholdBreachInsightService.java
@@ -1,5 +1,20 @@
 package com.springboot.connectmate.services;
 
+import com.springboot.connectmate.dtos.ThresholdBreachInsight.ThresholdBreachInsightDetailDTO;
+import com.springboot.connectmate.enums.ConnectMetricCode;
+import com.springboot.connectmate.enums.ConnectMetricType;
+import com.springboot.connectmate.enums.Status;
+import com.springboot.connectmate.models.Metric;
+import com.springboot.connectmate.models.ThresholdBreachInsight;
+
+import java.util.List;
+
 public interface ThresholdBreachInsightService {
+    ThresholdBreachInsight getInsightByMetricCodeAndConnectItemId(Metric metric, String connectItemId);
+    List<ThresholdBreachInsightDetailDTO> getAllInsights();
+    List<ThresholdBreachInsightDetailDTO> getInsightsByStatus(Status status);
+    List<ThresholdBreachInsightDetailDTO> getInsightsByConnectItemId(String connectItemId);
+    List<ThresholdBreachInsightDetailDTO> getInsightsByItemType(ConnectMetricType connectItemType);
+    String updateStatus(Long id, Status status);
 
 }
