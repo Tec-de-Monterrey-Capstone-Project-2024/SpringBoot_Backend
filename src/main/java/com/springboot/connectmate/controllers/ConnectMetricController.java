ea1a4d096d168096436b0382c5431091ae0ad277
abeejita <a01655892@itesm.com>
2024-05-29 19:53:45 -0600

    Fix metric retrieval for queues and agents.

--- src/main/java/com/springboot/connectmate/controllers/ConnectMetricController.java
+++ src/main/java/com/springboot/connectmate/controllers/ConnectMetricController.java
@@ -40,10 +40,10 @@ public class ConnectMetricController {
             summary = "Get all queue metrics of a particular queue.",
             description = "Get Amazon Connect queue metrics by instance ID and queue ID."
     )
-    @GetMapping("/instances/{instanceArn}/queue/{queueId}/metrics")
+    @GetMapping("/instances/queue-metrics")
     public ResponseEntity<ConnectQueueMetricDTO> getQueueMetrics(
-            @PathVariable(name = "instanceArn") String instanceArn,
-            @PathVariable(name = "queueId") String queueId
+            @RequestParam(name = "instanceArn") String instanceArn,
+            @RequestParam(name = "queueId") String queueId
     ){
         return ResponseEntity.ok(amazonConnectService.getQueueMetrics(instanceArn, queueId));
     }
@@ -87,10 +87,10 @@ public class ConnectMetricController {
             summary = "Get all agent metrics",
             description = "Get Amazon Connect metrics by instanceARN and agentId."
     )
-    @GetMapping("/instances/{instanceArn}/agent/{agentId}/metrics")
+    @GetMapping("/instances/agent-metrics")
     public ResponseEntity<ConnectAgentMetricDTO> getAgentMetrics(
-            @PathVariable(name = "instanceArn") String instanceArn,
-            @PathVariable(name = "agentId") String agentId) {
+            @RequestParam(name = "instanceArn") String instanceArn,
+            @RequestParam(name = "agentId") String agentId) {
         return ResponseEntity.ok(amazonConnectService.getAgentMetrics(instanceArn, agentId));
     }
 
