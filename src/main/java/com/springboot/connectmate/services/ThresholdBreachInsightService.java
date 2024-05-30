167e8701d8ee5c6bc109eaef37492d7893922696
Aram32mm <a01657142@tec.mx>
2024-05-29 18:28:33 -0600

    Update ThresholdBreachInsightService.java

--- src/main/java/com/springboot/connectmate/services/ThresholdBreachInsightService.java
+++ src/main/java/com/springboot/connectmate/services/ThresholdBreachInsightService.java
@@ -11,11 +11,17 @@ import java.util.List;
 
 
 public interface ThresholdBreachInsightService {
+
+    // Internal Use Only
     ThresholdBreachInsight generateAndSaveInsight(ThresholdBreachInsightDetailDTO dto, InsightDTO insight);
     ThresholdBreachInsight getInsightByMetricCodeAndConnectItemId(Metric metric, String connectItemId);
+
+    // Get all insights (filter also)
     List<ThresholdBreachInsightDetailDTO> getAllInsights();
     List<ThresholdBreachInsightDetailDTO> getInsightsByStatus(Status status);
     List<ThresholdBreachInsightDetailDTO> getInsightsByConnectItemId(String connectItemId);
     List<ThresholdBreachInsightDetailDTO> getInsightsByItemType(ConnectMetricType connectItemType);
+
+    // Update the status of the insight
     String updateStatus(Long id, Status status);
 }
