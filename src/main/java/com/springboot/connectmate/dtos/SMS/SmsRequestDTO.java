f8d1e7c770c17af2011816da7e997b71f31653e0
Aram32mm <a01657142@tec.mx>
2024-05-30 08:09:12 -0600

    env vars and reninements for sns and email

--- src/main/java/com/springboot/connectmate/dtos/SMS/SmsRequestDTO.java
+++ src/main/java/com/springboot/connectmate/dtos/SMS/SmsRequestDTO.java
@@ -1,18 +1,29 @@
 package com.springboot.connectmate.dtos.SMS;
 
-import lombok.Getter;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Schema(
+        name = "SmsRequestDTO",
+        description = "DTO for sending SMS"
+)
 public class SmsRequestDTO {
 
+    @Schema(
+            description = "Phone number to send SMS",
+            example = "1234567890"
+    )
     private String phoneNumber;
+
+    @Schema(
+            description = "Message to send",
+            example = "Hello, this is a test message"
+    )
     private String message;
 
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
