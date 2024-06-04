30459b175ae0cbf82127dc096e6d4a92c0d206b5
fernando2504 <a01642285@tec.mx>
2024-06-04 13:03:48 -0600

    Create ConnectUserControllerTests.java

---
+++ src/test/java/com/springboot/connectmate/integration/ConnectUserControllerTests.java
@@ -0,0 +1,110 @@
+package com.springboot.connectmate.integration;
+
+import com.amazonaws.services.connect.model.AgentStatusSummary;
+import com.amazonaws.services.connect.model.Queue;
+import com.amazonaws.services.connect.model.RoutingProfileSummary;
+import com.amazonaws.services.connect.model.User;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springboot.connectmate.dtos.AmazonConnect.ConnectCurrentUserDataDTO;
+import com.springboot.connectmate.dtos.AmazonConnect.ConnectQueueDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Testcontainers
+@TestPropertySource({
+        "classpath:application-db-test.properties",
+        "classpath:application-aws-test.properties"
+})
+
+public class ConnectUserControllerTests {
+
+    @Container
+    private static MySQLContainer mysqlContainer = new MySQLContainer("mysql:latest")
+            .withDatabaseName("test_connectmate_db")
+            .withUsername("test_user")
+            .withPassword("test_password");
+    static {
+        mysqlContainer.start();
+        System.setProperty("JDBC_URL", mysqlContainer.getJdbcUrl());
+        System.setProperty("DB_NAME", "test_connectmate_db");
+        System.setProperty("DB_USER", "test_user");
+        System.setProperty("DB_PASSWORD", "test_password");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void givenInstanceIdAndUserId_whenGetUserDescription_thenReturnUser() throws Exception {
+        // given - precondition or setup
+        String instanceId = "7c78bd60-4a9f-40e5-b461-b7a0dfaad848";
+        String userId = "ed1ad50d-2ffc-44ad-a565-71f13ad991a5";
+        User expectedUser = new User();
+
+        // when - action or behaviour tested
+        ResultActions response = mockMvc.perform(get("/api/amazon-connect/instances/{instanceId}/users/{userId}/description", instanceId,userId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(expectedUser))
+        ).andExpect(status().isOk()
+        ).andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenInstanceId_whenGetRoutingProfile_thenReturnRoutingProfile() throws Exception {
+        String instanceId = "7c78bd60-4a9f-40e5-b461-b7a0dfaad848";
+        List<RoutingProfileSummary> expectedRoutingProfile = Collections.singletonList(new RoutingProfileSummary());
+
+        ResultActions response = mockMvc.perform(get("/api/amazon-connect/instances/{instanceId}/routing-profiles", instanceId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(expectedRoutingProfile))
+        ).andExpect(status().isOk()
+        ).andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
+        // then - verify the result or output using assert statements
+        response.andDo(print())
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    public void givenInstanceId_whenGetAgentStatuses_thenReturnAgentStatuses() throws Exception {
+        String instanceId = "7c78bd60-4a9f-40e5-b461-b7a0dfaad848";
+        List<AgentStatusSummary> expectedStatusSummary = Collections.singletonList(new AgentStatusSummary());
+
+        ResultActions response = mockMvc.perform(get("/api/amazon-connect/instances/{instanceId}/agent-statuses", instanceId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(expectedStatusSummary))
+        ).andExpect(status().isOk()
+        ).andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+        // then - verify the result or output using assert statements
+        response.andDo(print())
+                .andExpect(status().isOk());
+
+    }
+}
