3837531c92bdd0c90aee695ecd000ecdea2b383c
Aram32mm <a01657142@tec.mx>
2024-06-04 17:47:55 -0600

    Update ConnectMetricControllerTests.java

--- src/test/java/com/springboot/connectmate/integration/ConnectMetricControllerTests.java
+++ src/test/java/com/springboot/connectmate/integration/ConnectMetricControllerTests.java
@@ -5,8 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -14,7 +18,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Testcontainers
+@TestPropertySource({
+        "classpath:application-db-test.properties",
+        "classpath:application-aws-test.properties"
+})
 public class ConnectMetricControllerTests {
+
+    @Container
+    private static MySQLContainer mysqlContainer = new MySQLContainer("mysql:latest")
+            .withDatabaseName("test_connectmate_db")
+            .withUsername("test_user")
+            .withPassword("test_password");
+    static {
+        mysqlContainer.start();
+        System.setProperty("JDBC_URL", mysqlContainer.getJdbcUrl());
+        System.setProperty("DB_NAME", "test_connectmate_db");
+        System.setProperty("DB_USER", "test_user");
+        System.setProperty("DB_PASSWORD", "test_password");
+    }
+
     @Autowired
     private MockMvc mockMvc;
 
