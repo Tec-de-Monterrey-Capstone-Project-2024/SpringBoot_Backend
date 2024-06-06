2837a85ce955877ee96c947bf663a110ceecf98c
Aram32mm <a01657142@tec.mx>
2024-06-05 19:23:26 -0600

    Delete bug fixed

--- src/main/java/com/springboot/connectmate/repositories/ThresholdBreachInsightRepository.java
+++ src/main/java/com/springboot/connectmate/repositories/ThresholdBreachInsightRepository.java
@@ -5,6 +5,8 @@ import com.springboot.connectmate.enums.Status;
 import com.springboot.connectmate.models.Metric;
 import com.springboot.connectmate.models.ThresholdBreachInsight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +18,7 @@ public interface ThresholdBreachInsightRepository extends JpaRepository<Threshol
     List<ThresholdBreachInsight> findByStatus(Status status);
     List<ThresholdBreachInsight> findByConnectItemId(String connectItemId);
     List<ThresholdBreachInsight> findByConnectItemType(ConnectMetricType connectItemType);
+    @Modifying
+    @Query("delete from ThresholdBreachInsight t where t.id = :id")
+    void deleteById(Long id);
 }
